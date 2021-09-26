5975448c1c46fe60f6c9872f9b80e773a7a309dd
Blue'Ose Tech world <blueosetechworld@gmail.com>
2021-09-26 16:23:38 +0530

    DATABASE UPdate

--- pharmaceutical/database.py
+++ pharmaceutical/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.environ['DATABASE_URL']
+DATABASE_URL = os.environ['CIPLA_DATABASE_URL']
 
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
